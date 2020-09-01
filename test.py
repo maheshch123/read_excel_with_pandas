00117c58c68924f6b8d3ed25b43088189838e8e1
maheshch123 <a1nusuz7>
2020-09-01 18:51:44 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+emails = input("enter email :")
+e = pd.read_excel("Email.xlsx")
+x = e['Emails'].values
+
+# print(emails)
+k=0
+for i in x:
+    if emails == i:
+        print('Email exists at row Number :',k+2)
+        # print(k+2)
+        # break
+    k+= 1
+
+
